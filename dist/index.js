efd3f6597d69d0ab41ddf959a054c43d844deae5
Lasse Nielsen <github@al1as.dk>
2023-12-16 17:26:02 +0100

    added option 'debug' to allow printing all input data and output request added option 'server_type' options [github, gitea] (default github), to exclude features not available on that platform

--- dist/index.js
+++ dist/index.js
@@ -37727,8 +37727,8 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(2186);
 const axios = __nccwpck_require__(8757);
 const github = __nccwpck_require__(5438);
-//const FormData = require('form-data');
-//const fs = require('fs');
+//const FormData = require("form-data");
+//const fs = require("fs");
 
 async function githubmessage() {
 	const context = github.context;
@@ -37737,25 +37737,26 @@ async function githubmessage() {
 	let message
 
 	switch (context.eventName) {
-		case 'push':
-			action_buttons = [{
-				"action": "view",
-				"label": "Compare",
-				"url": payload.compare,
-				"clear": true
-			},
-			{
-				"action": "view",
-				"label": "View Commit",
-				"url": payload.head_commit.url,
-				"clear": true
-			},
-			{
-				"action": "view",
-				"label": "Visit Repository",
-				"url": payload.repository.html_url,
-				"clear": true
-			}
+		case "push":
+			action_buttons = [
+				{
+					"action": "view",
+					"label": "Compare",
+					"url": payload.compare,
+					"clear": true
+				},
+				{
+					"action": "view",
+					"label": "View Commit",
+					"url": payload.head_commit.url,
+					"clear": true
+				},
+				{
+					"action": "view",
+					"label": "Visit Repository",
+					"url": payload.repository.html_url,
+					"clear": true
+				}
 			]
 
 			message = `${payload.head_commit.committer.name} has pushed ${context.sha.slice(-7)} to ${payload.repository.full_name}.\n\n` +
@@ -37769,25 +37770,26 @@ async function githubmessage() {
 
 			return [action_buttons, message]
 
-		case 'release':
-			action_buttons = [{
-				"action": "view",
-				"label": "Release URL",
-				"url": payload.release.html_url,
-				"clear": true
-			},
-			{
-				"action": "view",
-				"label": "Download Tar",
-				"url": payload.release.tarball_url,
-				"clear": true
-			},
-			{
-				"action": "view",
-				"label": "Download Zip",
-				"url": payload.release.zipball_url,
-				"clear": true
-			}
+		case "release":
+			action_buttons = [
+				{
+					"action": "view",
+					"label": "Release URL",
+					"url": payload.release.html_url,
+					"clear": true
+				},
+				{
+					"action": "view",
+					"label": "Download Tar",
+					"url": payload.release.tarball_url,
+					"clear": true
+				},
+				{
+					"action": "view",
+					"label": "Download Zip",
+					"url": payload.release.zipball_url,
+					"clear": true
+				}
 			]
 
 			message = `${payload.release.author.login} has ${payload.action} ${payload.release.tag_name} on ${payload.repository.full_name}.\n\n` +
@@ -37801,43 +37803,45 @@ async function githubmessage() {
 
 			return [action_buttons, message]
 
-		case 'schedule':
-			action_buttons = [{
-				"action": "view",
-				"label": "Visit Repository",
-				"url": `https://github.com/${process.env.GITHUB_REPOSITORY}`,
-				"clear": true
-			},
-			{
-				"action": "view",
-				"label": "View Run",
-				"url": `${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}`,
-				"clear": true
-			}
+		case "schedule":
+			action_buttons = [
+				{
+					"action": "view",
+					"label": "Visit Repository",
+					"url": `https://github.com/${process.env.GITHUB_REPOSITORY}`,
+					"clear": true
+				},
+				{
+					"action": "view",
+					"label": "View Run",
+					"url": `${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}`,
+					"clear": true
+				}
 			]
 
-			message = `Scheduled task '${context.job}' ran in ${process.env.GITHUB_REPOSITORY}.\n\n` +
+			message = `Scheduled task "${context.job}" ran in ${process.env.GITHUB_REPOSITORY}.\n\n` +
 				`Workflow Name: ${context.workflow}\n` +
 				`Cron: ${context.payload.schedule}`;
 
 			return [action_buttons, message]
 
 		default:
-			action_buttons = [{
-				"action": "view",
-				"label": "Visit Repo",
-				"url": payload.repository.html_url,
-				"clear": true
-			},
-			{
-				"action": "view",
-				"label": "View Run",
-				"url": `${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}`,
-				"clear": true
-			}
+			action_buttons = [
+				{
+					"action": "view",
+					"label": "Visit Repo",
+					"url": payload.repository.html_url,
+					"clear": true
+				},
+				{
+					"action": "view",
+					"label": "View Run",
+					"url": `${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}`,
+					"clear": true
+				}
 			]
 
-			message = `Workflow '${context.workflow}' ran in ${payload.repository.full_name}\n\n` +
+			message = `Workflow "${context.workflow}" ran in ${payload.repository.full_name}\n\n` +
 				`Repository: ${payload.repository.full_name}\n` +
 				`Workflow Job Name: ${context.job}\n` +
 				`Event Name: ${context.eventName}`
@@ -37848,64 +37852,58 @@ async function githubmessage() {
 
 async function run() {
 	try {
-		const url = core.getInput('url')
-		const headers = JSON.parse(core.getInput('headers') || '{}')
-		const tags = core.getInput('tags').split(',');
-		const topic = core.getInput('topic')
-		const title = core.getInput('title') || 'GitHub Actions'
-		const details = core.getInput('details')
-		const priority = core.getInput('priority') || 3
-		//const data = JSON.parse(core.getInput('data') || '{}')
-		//const name = core.getInput('name')
-		//const file = core.getInput('file')
+		const debug = core.getInput("debug") || false
+		const server_type = core.getInput("server_type") || "github"
+		const url = core.getInput("url")
+		const headers = JSON.parse(core.getInput("headers") || "{}")
+		const tags = core.getInput("tags").split(",");
+		const topic = core.getInput("topic")
+		const title = core.getInput("title") || "GitHub Actions"
+		const details = core.getInput("details")
+		const priority = core.getInput("priority") || 3
 
-		console.log(`URL: ${url}`)
-		console.log(`Headers: ${headers}`)
-		console.log(`Tags: ${tags}`)
-		console.log(`Topic: ${topic}`)
-		console.log(`Title: ${title}`)
-		console.log(`Details: ${details}`)
-		console.log(`Priority: ${priority}`)
-		//console.log(`Data: ${data}`)
-		//console.log(`File Name: ${name}`)
-		//console.log(`File Path: ${file}`)
-
-		//const form = new FormData();
-		//for (const [key, value] of Object.entries(data)) {
-		//  form.append(key, value)
-		//}
-
-		//if (name && file) {
-		//  form.append(name, fs.createReadStream(file));
-		//}
+		if (debug) {
+			console.log(`URL: ${url}`)
+			console.log(`Headers: ${JSON.stringify(headers)}`)
+			console.log(`Tags: ${tags}`)
+			console.log(`Topic: ${topic}`)
+			console.log(`Title: ${title}`)
+			console.log(`Details: ${details}`)
+			console.log(`Priority: ${priority}`)
+		}
 
 		core.info(`Connecting to endpoint (${url}) ...`)
 
 		let message = await githubmessage();
+
+		if (server_type == "gitea") {
+			delete message[0][0]
+		}
+
 		let request = {
-			method: 'POST',
+			method: "POST",
 			url: url,
 			headers: {
-				'Content-Type': 'application/json',
-				'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0',
-				'Priority': priority,
+				"Content-Type": "application/json",
+				"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0",
+				"Priority": priority,
 				...headers
 			},
 			data: JSON.stringify({
-				'topic': topic,
-				'tags': tags,
-				'title': (title),
-				'actions': message[0],
+				"topic": topic,
+				"tags": tags,
+				"title": (title),
+				"actions": message[0],
 				"message": message[1] + "\n\n" + details
 			})
-
 		};
-		console.log(request);
+		if (debug)
+			console.log(request);
 
 		const response = await axios(request);
 
-		core.setOutput('response', {
-			'statusCode': response.statusCode
+		core.setOutput("response", {
+			"statusCode": response.statusCode
 		});
 	} catch (error) {
 		console.log(error.response.data)
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-const core = require('@actions/core');
-const axios = require('axios');
-const github = require('@actions/github');
-//const FormData = require('form-data');
-//const fs = require('fs');
+const core = require("@actions/core");
+const axios = require("axios");
+const github = require("@actions/github");
+//const FormData = require("form-data");
+//const fs = require("fs");
 
 async function githubmessage() {
 	const context = github.context;
@@ -11,25 +11,26 @@ async function githubmessage() {
 	let message
 
 	switch (context.eventName) {
-		case 'push':
-			action_buttons = [{
-				"action": "view",
-				"label": "Compare",
-				"url": payload.compare,
-				"clear": true
-			},
-			{
-				"action": "view",
-				"label": "View Commit",
-				"url": payload.head_commit.url,
-				"clear": true
-			},
-			{
-				"action": "view",
-				"label": "Visit Repository",
-				"url": payload.repository.html_url,
-				"clear": true
-			}
+		case "push":
+			action_buttons = [
+				{
+					"action": "view",
+					"label": "Compare",
+					"url": payload.compare,
+					"clear": true
+				},
+				{
+					"action": "view",
+					"label": "View Commit",
+					"url": payload.head_commit.url,
+					"clear": true
+				},
+				{
+					"action": "view",
+					"label": "Visit Repository",
+					"url": payload.repository.html_url,
+					"clear": true
+				}
 			]
 
 			message = `${payload.head_commit.committer.name} has pushed ${context.sha.slice(-7)} to ${payload.repository.full_name}.\n\n` +
@@ -43,25 +44,26 @@ async function githubmessage() {
 
 			return [action_buttons, message]
 
-		case 'release':
-			action_buttons = [{
-				"action": "view",
-				"label": "Release URL",
-				"url": payload.release.html_url,
-				"clear": true
-			},
-			{
-				"action": "view",
-				"label": "Download Tar",
-				"url": payload.release.tarball_url,
-				"clear": true
-			},
-			{
-				"action": "view",
-				"label": "Download Zip",
-				"url": payload.release.zipball_url,
-				"clear": true
-			}
+		case "release":
+			action_buttons = [
+				{
+					"action": "view",
+					"label": "Release URL",
+					"url": payload.release.html_url,
+					"clear": true
+				},
+				{
+					"action": "view",
+					"label": "Download Tar",
+					"url": payload.release.tarball_url,
+					"clear": true
+				},
+				{
+					"action": "view",
+					"label": "Download Zip",
+					"url": payload.release.zipball_url,
+					"clear": true
+				}
 			]
 
 			message = `${payload.release.author.login} has ${payload.action} ${payload.release.tag_name} on ${payload.repository.full_name}.\n\n` +
@@ -75,43 +77,45 @@ async function githubmessage() {
 
 			return [action_buttons, message]
 
-		case 'schedule':
-			action_buttons = [{
-				"action": "view",
-				"label": "Visit Repository",
-				"url": `https://github.com/${process.env.GITHUB_REPOSITORY}`,
-				"clear": true
-			},
-			{
-				"action": "view",
-				"label": "View Run",
-				"url": `${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}`,
-				"clear": true
-			}
+		case "schedule":
+			action_buttons = [
+				{
+					"action": "view",
+					"label": "Visit Repository",
+					"url": `https://github.com/${process.env.GITHUB_REPOSITORY}`,
+					"clear": true
+				},
+				{
+					"action": "view",
+					"label": "View Run",
+					"url": `${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}`,
+					"clear": true
+				}
 			]
 
-			message = `Scheduled task '${context.job}' ran in ${process.env.GITHUB_REPOSITORY}.\n\n` +
+			message = `Scheduled task "${context.job}" ran in ${process.env.GITHUB_REPOSITORY}.\n\n` +
 				`Workflow Name: ${context.workflow}\n` +
 				`Cron: ${context.payload.schedule}`;
 
 			return [action_buttons, message]
 
 		default:
-			action_buttons = [{
-				"action": "view",
-				"label": "Visit Repo",
-				"url": payload.repository.html_url,
-				"clear": true
-			},
-			{
-				"action": "view",
-				"label": "View Run",
-				"url": `${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}`,
-				"clear": true
-			}
+			action_buttons = [
+				{
+					"action": "view",
+					"label": "Visit Repo",
+					"url": payload.repository.html_url,
+					"clear": true
+				},
+				{
+					"action": "view",
+					"label": "View Run",
+					"url": `${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}`,
+					"clear": true
+				}
 			]
 
-			message = `Workflow '${context.workflow}' ran in ${payload.repository.full_name}\n\n` +
+			message = `Workflow "${context.workflow}" ran in ${payload.repository.full_name}\n\n` +
 				`Repository: ${payload.repository.full_name}\n` +
 				`Workflow Job Name: ${context.job}\n` +
 				`Event Name: ${context.eventName}`
@@ -122,64 +126,58 @@ async function githubmessage() {
 
 async function run() {
 	try {
-		const url = core.getInput('url')
-		const headers = JSON.parse(core.getInput('headers') || '{}')
-		const tags = core.getInput('tags').split(',');
-		const topic = core.getInput('topic')
-		const title = core.getInput('title') || 'GitHub Actions'
-		const details = core.getInput('details')
-		const priority = core.getInput('priority') || 3
-		//const data = JSON.parse(core.getInput('data') || '{}')
-		//const name = core.getInput('name')
-		//const file = core.getInput('file')
+		const debug = core.getInput("debug") || false
+		const server_type = core.getInput("server_type") || "github"
+		const url = core.getInput("url")
+		const headers = JSON.parse(core.getInput("headers") || "{}")
+		const tags = core.getInput("tags").split(",");
+		const topic = core.getInput("topic")
+		const title = core.getInput("title") || "GitHub Actions"
+		const details = core.getInput("details")
+		const priority = core.getInput("priority") || 3
 
-		console.log(`URL: ${url}`)
-		console.log(`Headers: ${headers}`)
-		console.log(`Tags: ${tags}`)
-		console.log(`Topic: ${topic}`)
-		console.log(`Title: ${title}`)
-		console.log(`Details: ${details}`)
-		console.log(`Priority: ${priority}`)
-		//console.log(`Data: ${data}`)
-		//console.log(`File Name: ${name}`)
-		//console.log(`File Path: ${file}`)
-
-		//const form = new FormData();
-		//for (const [key, value] of Object.entries(data)) {
-		//  form.append(key, value)
-		//}
-
-		//if (name && file) {
-		//  form.append(name, fs.createReadStream(file));
-		//}
+		if (debug) {
+			console.log(`URL: ${url}`)
+			console.log(`Headers: ${JSON.stringify(headers)}`)
+			console.log(`Tags: ${tags}`)
+			console.log(`Topic: ${topic}`)
+			console.log(`Title: ${title}`)
+			console.log(`Details: ${details}`)
+			console.log(`Priority: ${priority}`)
+		}
 
 		core.info(`Connecting to endpoint (${url}) ...`)
 
 		let message = await githubmessage();
+
+		if (server_type == "gitea") {
+			delete message[0][0]
+		}
+
 		let request = {
-			method: 'POST',
+			method: "POST",
 			url: url,
 			headers: {
-				'Content-Type': 'application/json',
-				'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0',
-				'Priority': priority,
+				"Content-Type": "application/json",
+				"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0",
+				"Priority": priority,
 				...headers
 			},
 			data: JSON.stringify({
-				'topic': topic,
-				'tags': tags,
-				'title': (title),
-				'actions': message[0],
+				"topic": topic,
+				"tags": tags,
+				"title": (title),
+				"actions": message[0],
 				"message": message[1] + "\n\n" + details
 			})
-
 		};
-		console.log(request);
+		if (debug)
+			console.log(request);
 
 		const response = await axios(request);
 
-		core.setOutput('response', {
-			'statusCode': response.statusCode
+		core.setOutput("response", {
+			"statusCode": response.statusCode
 		});
 	} catch (error) {
 		console.log(error.response.data)
